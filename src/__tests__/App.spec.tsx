a802495c2d39dfec24ffc2f815cbbebf76ff9085
Chatmalow <maxime.pie.mail@gmail.com>
2022-03-30 14:37:56 +0200

    config(front): Configure eslint to use airbnb coding style standards

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/react"
-import App from "../pages"
+import { render, screen } from '@testing-library/react';
+import App from '../pages';
 
-describe("App", () => {
-  it("should render correctly App", () => {
-    render(<App />)
+describe('App', () => {
+  it('should render correctly App', () => {
+    render(<App />);
     expect(
-      screen.getByText(/Welcome/)
-    ).toBeInTheDocument()
-  })
-})
+      screen.getByText(/Welcome/),
+    ).toBeInTheDocument();
+  });
+});

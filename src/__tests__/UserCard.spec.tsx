f50fd6ff42c3a8683011239708267347043ba079
Chatmalow <maxime.pie.mail@gmail.com>
2022-04-01 21:22:54 +0200

    tests (UserCard): Add UserCards tests

---
+++ src/__tests__/UserCard.spec.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { describe, it, expect } from '@jest/globals';
+import { render, screen } from '@testing-library/react';
+import UserCard from '../components/molecules/UserCard';
+
+const user = {
+  id: 1,
+  nickname: 'Thibaut',
+  token: 'xxxx',
+  image: 'https://www.nautiljon.com/images/perso/00/81/franky_1818.jpg',
+};
+
+describe('User Card', () => {
+  it('Display the good name', () => {
+    render(<UserCard user={user} />);
+    // @ts-ignore
+    expect(screen.findByText(/Thibault/)).toBeInTheDocument;
+  });
+});

2c52dff419dd95a153663db589a30136b4cc6725
Chatmalow <maxime.pie.mail@gmail.com>
2022-04-01 14:32:40 +0200

    test(Message): Add tests for Message component

---
+++ src/__tests__/Message.spec.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { describe, it, expect } from '@jest/globals';
+import { render, screen } from '@testing-library/react';
+import Message from '../components/atoms/Message';
+import { getLoggedUserId } from '../utils/getLoggedUserId';
+import { Message as MessageType } from '../types/message';
+
+const messageSentByDonkey: MessageType = {
+  id: 5,
+  conversationId: 1,
+  timestamp: 1625648867,
+  authorId: 2,
+  body: "I can relate. I'm too used to React Router. \uD83D\uDE2D",
+};
+
+const messageSentByUser: MessageType = {
+  id: 6,
+  conversationId: 1,
+  timestamp: 1625648867,
+  authorId: 1,
+  body: 'OH! What a day!',
+};
+
+const contact = 'Donkey';
+
+describe('Message', () => {
+  it('should display the username of the sender if different from connected user', () => {
+    render(
+      <Message
+        message={messageSentByDonkey}
+        contactUsername={contact}
+        isSentByUser={getLoggedUserId() === messageSentByDonkey.authorId}
+      />,
+    );
+
+    expect(
+      screen.getByText(/Donkey/),
+    ).toBeInTheDocument();
+  });
+
+  it('should display differently if message is written by the connected user', () => {
+    render(
+      <Message
+        message={messageSentByUser}
+        contactUsername={contact}
+        isSentByUser={getLoggedUserId() === messageSentByUser.authorId}
+      />,
+    );
+    const messageElement = document.querySelector('.Message');
+    const hasModifier = messageElement.classList.contains('Message__from-logged-user');
+    expect(hasModifier).toBeTruthy();
+  });
+
+  it('should not display differently if message is not written by connected user', () => {
+    render(
+      <Message
+        message={messageSentByDonkey}
+        contactUsername={contact}
+        isSentByUser={getLoggedUserId() === messageSentByDonkey.authorId}
+      />,
+    );
+    const messageElement = document.querySelector('.Message');
+    const hasModifier = messageElement.classList.contains('Message__from-logged-user');
+    expect(hasModifier).toBeFalsy();
+  });
+});
